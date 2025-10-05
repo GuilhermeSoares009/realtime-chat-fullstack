93a43e4ada65b48fb525f81e79ee41ad2a1af4ae
Guilherme Soares <guilherme.sousa009@hotmail.com>
2025-10-04 21:50:29 -0300

    feat: ui contacts-search

--- frontend/components/Contacts.jsx
+++ frontend/components/Contacts.jsx
@@ -32,24 +32,11 @@ const Contacts = () => {
   }, [currentUser, search]);
 
   /* SELECT CONTACT */
-  const [selectedContacts, setSelectedContacts] = useState([]);
-  const isGroup = selectedContacts.length > 1;
+  const [selectedContact, setSelectedContact] = useState(null);
 
   const handleSelect = (contact) => {
-    if (selectedContacts.includes(contact)) {
-      setSelectedContacts((prevSelectedContacts) =>
-        prevSelectedContacts.filter((item) => item !== contact)
-      );
-    } else {
-      setSelectedContacts((prevSelectedContacts) => [
-        ...prevSelectedContacts,
-        contact,
-      ]);
-    }
+    setSelectedContact(contact);
   };
-
-  /* ADD GROUP CHAT NAME */
-  const [name, setName] = useState("");
 
   const router = useRouter();
 
@@ -59,9 +46,9 @@ const Contacts = () => {
       method: "POST",
       body: JSON.stringify({
         currentUserId: currentUser._id,
-        members: selectedContacts.map((contact) => contact._id),
-        isGroup,
-        name,
+        members: [selectedContact._id],
+        isGroup: false,
+        name: "",
       }),
     });
     const chat = await res.json();
@@ -84,7 +71,7 @@ const Contacts = () => {
 
       <div className="contact-bar">
         <div className="contact-list">
-          <p className="text-body-bold">Select or Deselect</p>
+          <p className="text-body-bold">Select Contact</p>
 
           <div className="flex flex-col flex-1 gap-5 overflow-y-scroll custom-scrollbar">
             {contacts.map((user, index) => (
@@ -93,7 +80,7 @@ const Contacts = () => {
                 className="contact"
                 onClick={() => handleSelect(user)}
               >
-                {selectedContacts.find((item) => item === user) ? (
+                {selectedContact === user ? (
                   <CheckCircle sx={{ color: "red" }} />
                 ) : (
                   <RadioButtonUnchecked />
@@ -110,36 +97,12 @@ const Contacts = () => {
         </div>
 
         <div className="create-chat">
-          {isGroup && (
-            <>
-              <div className="flex flex-col gap-3">
-                <p className="text-body-bold">Group Chat Name</p>
-                <input
-                  placeholder="Enter group chat name..."
-                  className="input-group-name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                />
-              </div>
-
-              <div className="flex flex-col gap-3">
-                <p className="text-body-bold">Members</p>
-                <div className="flex flex-wrap gap-3">
-                  {selectedContacts.map((contact, index) => (
-                    <p className="selected-contact" key={index}>
-                      {contact.username}
-                    </p>
-                  ))}
-                </div>
-              </div>
-            </>
-          )}
           <button
             className="btn"
             onClick={createChat}
-            disabled={selectedContacts.length === 0}
+            disabled={!selectedContact}
           >
-            FIND OR START A NEW CHAT
+            START CHAT
           </button>
         </div>
       </div>
