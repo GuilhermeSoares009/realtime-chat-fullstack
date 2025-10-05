d41c0f916d051f93026c7763b07c523bccb9dc04
Guilherme Soares <guilherme.sousa009@hotmail.com>
2025-10-04 22:13:48 -0300

    feat: chat detail

--- frontend/components/MessageBox.jsx
+++ frontend/components/MessageBox.jsx
@@ -3,7 +3,7 @@ import { format } from "date-fns"
 const MessageBox = ({ message, currentUser }) => {
   return message?.sender?._id !== currentUser._id ? (
     <div className="message-box">
-      <img src={message?.sender?.profileImage || "/assets/person.jpg"} alt="profile photo" className="message-profilePhoto" />
+      <img src="https://api.dicebear.com/9.x/adventurer/svg?seed=Jack" alt="profile photo" className="message-profilePhoto" />
       <div className="message-info">
         <p className="text-small-bold">
           {message?.sender?.username} &#160; &#183; &#160; {format(new Date(message?.createdAt), "p")}
@@ -12,7 +12,7 @@ const MessageBox = ({ message, currentUser }) => {
         {message?.text ? (
           <p className="message-text">{message?.text}</p>
         ) : (
-          <img src={message?.photo} alt="message" className="message-photo" />
+          <img src="https://api.dicebear.com/9.x/adventurer/svg?seed=Jack" alt="message" className="message-photo" />
         )}
       </div>
     </div>
@@ -26,7 +26,7 @@ const MessageBox = ({ message, currentUser }) => {
         {message?.text ? (
           <p className="message-text-sender">{message?.text}</p>
         ) : (
-          <img src={message?.photo} alt="message" className="message-photo" />
+          <img src="https://api.dicebear.com/9.x/adventurer/svg?seed=Robert" alt="message" className="message-photo" />
         )}
       </div>
     </div>
